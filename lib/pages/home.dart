562245d7f8511a32a4fcd1c2d548c34eb12dd1f4
Say Glenn <glennsay1234@gmail.com>
2024-05-31 11:44:11 +0800

    Created Homepage

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
 
+  static const List<Widget> pages = [];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/home.svg',
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/icons/tasks.svg'),
+            label: 'Tasks',
+          ),
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/icons/settings.svg'),
+              label: 'Settings')
+        ],
+        onTap: (index) => {},
+      ),
     );
   }
 }
