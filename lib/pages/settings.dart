ba9c522a71564daffe7d209d1126f0f0717ef6f5
darylkoh <daryl.koh,hb@gmail.com>
2024-05-31 17:05:19 +0800

    Sign out button

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:edugalaxy/pages/login_page.dart';
 import 'package:flutter_svg/svg.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
 
   @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  Future<void> _signOut() async {
+    try {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => LoginPage()),
+      ); 
+    } catch (e) {
+      print(e);
+    }
+  }
+
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(''),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: _signOut,
+          child: Text('Sign out'),
+        ),
+      ),
     );
   }
 }
