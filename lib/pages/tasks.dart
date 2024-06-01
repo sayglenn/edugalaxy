9357344b9fb83cf6beb1a78cd1d4f3ce9d928c69
Say Glenn <glennsay1234@gmail.com>
2024-06-02 05:29:55 +0800

    Implemented time field

--- lib/pages/tasks.dart
+++ lib/pages/tasks.dart
@@ -12,7 +12,8 @@ class TasksPage extends StatelessWidget {
     final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
     String? title;
     DateTime? date;
-    int? timeToComplete;
+    int? hours;
+    int? minutes;
     Priority? priority = Priority.medium;
 
     return Column(
@@ -35,7 +36,7 @@ class TasksPage extends StatelessWidget {
                     builder: (BuildContext context) {
                       return Container(
                         width: double.infinity,
-                        height: 475,
+                        height: 525,
                         color: Color.fromARGB(255, 206, 238, 255),
                         child: Column(
                           mainAxisSize: MainAxisSize.min,
@@ -55,89 +56,10 @@ class TasksPage extends StatelessWidget {
                               child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                      top: 16.0,
-                                      left: 16.0,
-                                      right: 16.0,
-                                      bottom: 32.0,
-                                    ),
-                                    child: TextFormField(
-                                      decoration: InputDecoration(
-                                        labelText: "Title *",
-                                        filled: true,
-                                        fillColor: Colors.white,
-                                        contentPadding:
-                                            const EdgeInsets.all(15),
-                                        hintText:
-                                            'What will you name your task?',
-                                        border: OutlineInputBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(15),
-                                          borderSide: BorderSide.none,
-                                        ),
-                                      ),
-                                      validator: (value) {
-                                        if (value == null || value.isEmpty) {
-                                          return "Please input a title";
-                                        }
-                                        return null;
-                                      },
-                                      onSaved: (value) {
-                                        title = value;
-                                      },
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                      left: 16.0,
-                                      right: 16.0,
-                                      bottom: 32.0,
-                                    ),
-                                    child: DateTimeFormField(
-                                      onChanged: (newDate) {
-                                        date = newDate;
-                                      },
-                                      firstDate: DateTime.now(),
-                                      lastDate: DateTime(2100),
-                                      decoration: InputDecoration(
-                                        labelText: "Due Date *",
-                                        filled: true,
-                                        fillColor: Colors.white,
-                                        contentPadding:
-                                            const EdgeInsets.all(15),
-                                        hintText: 'When is the task due?',
-                                        border: OutlineInputBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(15),
-                                          borderSide: BorderSide.none,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                      left: 16.0,
-                                      right: 16.0,
-                                      bottom: 32.0,
-                                    ),
-                                    child: TextFormField(
-                                      decoration: InputDecoration(
-                                        labelText: "Duration *",
-                                        filled: true,
-                                        fillColor: Colors.white,
-                                        contentPadding:
-                                            const EdgeInsets.all(15),
-                                        hintText:
-                                            'How long does your task need?',
-                                        border: OutlineInputBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(15),
-                                          borderSide: BorderSide.none,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
+                                  _titleField(title),
+                                  _dueDateField(date),
+                                  _hoursField(hours),
+                                  _minutesField(minutes),
                                   Padding(
                                     padding: const EdgeInsets.only(
                                       left: 16.0,
@@ -176,6 +98,140 @@ class TasksPage extends StatelessWidget {
           ],
         ),
       ],
+    );
+  }
+
+  Padding _minutesField(int? minutes) {
+    return Padding(
+        padding: const EdgeInsets.only(
+          left: 16.0,
+          right: 16.0,
+          bottom: 32.0,
+        ),
+        child: DropdownButtonFormField<int>(
+          decoration: InputDecoration(
+            labelText: "Minutes *",
+            filled: true,
+            fillColor: Colors.white,
+            contentPadding: const EdgeInsets.all(15),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+              borderSide: BorderSide.none,
+            ),
+          ),
+          items: const [
+            DropdownMenuItem(
+              value: 0,
+              child: Text("0 minutes"),
+            ),
+            DropdownMenuItem(
+              value: 15,
+              child: Text("15 minutes"),
+            ),
+            DropdownMenuItem(
+              value: 30,
+              child: Text("30 minutes"),
+            ),
+            DropdownMenuItem(
+              value: 45,
+              child: Text("45 minutes"),
+            ),
+          ],
+          onChanged: (value) {
+            minutes = value;
+          },
+        ));
+  }
+
+  Padding _hoursField(int? hours) {
+    return Padding(
+        padding: const EdgeInsets.only(
+          left: 16.0,
+          right: 16.0,
+          bottom: 32.0,
+        ),
+        child: DropdownButtonFormField<int>(
+          decoration: InputDecoration(
+            labelText: "Hours *",
+            filled: true,
+            fillColor: Colors.white,
+            contentPadding: const EdgeInsets.all(15),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(15),
+              borderSide: BorderSide.none,
+            ),
+          ),
+          items: List.generate(
+              8,
+              (index) => DropdownMenuItem(
+                    value: index + 1,
+                    child:
+                        Text('${index + 1} hour${index + 1 == 1 ? "" : "s"}'),
+                  )),
+          onChanged: (value) {
+            hours = value;
+          },
+        ));
+  }
+
+  Padding _dueDateField(DateTime? date) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 16.0,
+        right: 16.0,
+        bottom: 32.0,
+      ),
+      child: DateTimeFormField(
+        onChanged: (newDate) {
+          date = newDate;
+        },
+        firstDate: DateTime.now(),
+        lastDate: DateTime(2100),
+        decoration: InputDecoration(
+          labelText: "Due Date *",
+          filled: true,
+          fillColor: Colors.white,
+          contentPadding: const EdgeInsets.all(15),
+          hintText: 'When is the task due?',
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: BorderSide.none,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Padding _titleField(String? title) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        top: 16.0,
+        left: 16.0,
+        right: 16.0,
+        bottom: 32.0,
+      ),
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: "Title *",
+          filled: true,
+          fillColor: Colors.white,
+          contentPadding: const EdgeInsets.all(15),
+          hintText: 'What will you name your task?',
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: BorderSide.none,
+          ),
+        ),
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return "Please input a title";
+          }
+          return null;
+        },
+        onSaved: (value) {
+          title = value;
+        },
+      ),
     );
   }
 }
